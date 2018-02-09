124cc0debb388c14de0b49fb697b2b194f71048d
Mark Soloff <35039498+Marksoloff@users.noreply.github.com>
2018-02-08 22:48:47 -0600

    Add files via upload

---
+++ draft 1d.py
@@ -0,0 +1,13 @@
+# First, I typed import random so I could use the random module.
+import random
+personality_of_bear = ("Rookie", "Washed up", "Retired", "Unhinged", "Slick", "Incompetent")
+stat1 = random.choice(personality_of_bear)
+bear_type = ("Grizzly Bear", "Polar Bear", "Panda", "Black Bear", "Sun Bear", "Honey Badger")
+stat2 = random.choice(bear_type)
+criminal_role_of_bear = ("Enforcer", "Mastermind", "Getaway Driver", "Hacker", "Thief", "Face")
+stat3 = random.choice(criminal_role_of_bear)
+print("Welcome to the gang, ya filthy animal. Ya remind me of my brudda. He was also a damned..."), print(stat1), print(stat2), print(stat3)
+
+#ToDo: script a command to run the whole series of expressions
+#      Create a start screen with "roll" button.
+#      Create a finish screen.
